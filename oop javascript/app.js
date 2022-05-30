610d31ed73557a85d7b026b2270c90cdc4547b01
stevenzct <97181113+stevenzct@users.noreply.github.com>
2022-05-30 21:00:25 +0800

    Add files via upload

---
+++ oop javascript/app.js
@@ -0,0 +1,33 @@
+const userOne = {
+    username: 'ryu',
+    email: 'ryu@thenetninja.co.uk',
+    login(){
+        console.log('the user logged in');
+    }, 
+    logout() {
+        console.log('the user logged out');
+    }
+};
+
+console.log(userOne.email, userOne.username);
+userOne.login();
+
+
+const userTwo = {
+    username: 'chun li',
+    email: 'chun li @thenetninja.co.uk',
+    login(){
+        console.log('the user logged in');
+    }, 
+    logout() {
+        console.log('the user logged out');
+    }
+};
+
+console.log(userTwo.email, userTwo.username);
+userTwo.login();
+
+
+
+
+const userThree = new User('shaun@thenetninja.co.uk', 'shaun');
