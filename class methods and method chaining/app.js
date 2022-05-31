558d67d4f1b4b973949a43df564222d4a4d0c11e
stevenzct <97181113+stevenzct@users.noreply.github.com>
2022-05-31 10:02:05 +0800

    Add files via upload

---
+++ class methods and method chaining/app.js
@@ -0,0 +1,29 @@
+class User {
+    constructor(username, email){
+        // set up properties
+        this.username = username;
+        this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
+    }
+}
+
+const userOne = new User('mario', 'mario@thenetninja.co.uk');
+const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+
+console.log(userOne, userTwo); 
+
+userOne.login().incScore().incScore().logout();
+
